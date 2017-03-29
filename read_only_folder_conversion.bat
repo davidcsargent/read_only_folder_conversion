e67ed928ecf4723a2e3f3f75939d7ad25312dc08
David Sargent <davidcsargent@users.noreply.github.com>
2017-03-29 09:44:59 +0100

    Create read_only_folder_conversion.bat
    Template batch file for converting all files in folder to read only.

---
+++ read_only_folder_conversion.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo Read Only Batch Change Facility
+echo for [enter details]
+:: CHANGES ATTRIBUTE to READ ONLY
+echo.
+attrib +r "C:\users\davidcsargent\Desktop\test\test1\*.*"
+
+pause
